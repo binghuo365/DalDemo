84f61f5730aad9d7fa6bd7f86943f8ae7e213405
binghuo365 <binghuo365@gmail.com>
2015-06-17 17:40:55 +0800

    mysql test

--- DalDemo/main.cpp
+++ DalDemo/main.cpp
@@ -1,6 +1,12 @@
+#include "DAFrame/DAOBASE/connectionmanager.h"
+#include "DAFrame/DAOBASE/poolconnectionmanager.h"
 
 int main(int argc, char* argv[])
 {
+	DAFrame::CConnectionManager::registProtocol("mysql", NULL);
+	DAFrame::CPoolConnectionManager::initialize("mysql://192.168.9.82:3306/gw_power", 50, "root", "root");
+	DAFrame::CPoolConnAutoPtr<DAFrame::CPoolConnection> conn
+		= DAFrame::CPoolConnectionManager::getConnection();
 	return 0;
 }
 
